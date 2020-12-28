d941c6284b972a39376400f6ffd02eb6c28029f1
javib15 <39830541+javib15@users.noreply.github.com>
2020-12-28 14:42:42 -0500

    cambias generalez

---
+++ contact.php
@@ -0,0 +1,58 @@
+<?php
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$message1 = $_POST['message-text'];
+
+$header .= 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message .= "Email send by : " .$_POST['name']. " \r\n";
+$message .= "Email " . $_POST['mail']. " \r\n";
+$message .= "Message: " . $_POST['message-text'] . " \r\n";
+$message .= "date message: " . date('d/m/Y', time());
+
+$para = "email@host.com";  //asegurate que el correo utilizado es parte de hosting donde tienes alijado la pagina
+$asunto = 'Contact ';
+
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+
+?>
+<!-- mensaje que fue enviado el correo  -->
+</h2>
+<h2 align="center">Thank you!</h2>
+
+<p align="center">Your message has been sent correctly, we will contact you soon.</p>
+<p align="center"> </p>
+<p><span style="color:red;font-size:150%;font-weight:bold;"><?php print $email; ?></span></p>
+
+<p align="center">If it is not correct,
+
+<script type='text/javascript'>
+
+document.write('<a href="https://www.avantikasxm.com/">go back</a>');
+
+</script>
+
+<noscript>go back</noscript> and send it again</p>
+
+<script type='text/javascript'>
+
+document.write('<p class="details"><a href="https://www.avantikasxm.com/">return to homepage.</a></p>');
+
+</script>
+
+<script type='text/javascript'>
+
+setTimeout('https://avantikasxm.com/', 9000);
+
+</script>
+
+<noscript>
+
+<p align="center" class="details">Press the "back" button on your browser to return to the previous page.</p>
+
+</noscript>
